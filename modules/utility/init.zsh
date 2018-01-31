a4900072d3bb8966d4109450a6aea38063109818
Millian Poquet <millian.poquet@inria.fr>
2018-01-31 17:44:01 +0100

    lu alias: ls -> llpp update

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -110,7 +110,7 @@ alias lx='ll -XB'        # Lists sorted by extension (GNU only).
 alias lk='ll -Sr'        # Lists sorted by size, largest last.
 alias lt='ll -tr'        # Lists sorted by date, most recent last.
 alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
-alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
+#alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
 alias sl='ls'            # I often screw this up.
 
 # Grep
